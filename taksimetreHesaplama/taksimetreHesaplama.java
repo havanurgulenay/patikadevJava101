c0a1716452fa49435c3436e59ac0154ee8603922
Havanur Gülenay <146188345+havanurgulenay@users.noreply.github.com>
2023-10-21 14:49:19 +0300

    Add files via upload

---
+++ taksimetreHesaplama/taksimetreHesaplama.java
@@ -0,0 +1,23 @@
+package taksimetreHesaplama;
+
+import java.util.Scanner;
+
+public class taksimetreHesaplama {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total, startPrice = 10;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Mesafeyi Giriniz(km): ");
+        km = inp.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+
+        System.out.println("Toplam tutar: " + total);
+
+
+    }
+}
