b7d5413865a0d6d8638c6545efd162457a78097f
Havanur Gülenay <146188345+havanurgulenay@users.noreply.github.com>
2024-02-03 22:14:22 +0300

    KullaniciGirisi.java

--- KullaniciGirisi/KullaniciGirisi.java
+++ KullaniciGirisi/KullaniciGirisi.java
@@ -13,17 +13,17 @@ public class KullaniciGirisi {
         System.out.print("Şifreniz : ");
         password = input.nextLine();
 
-        if(userName.equals("hrglny") && password.equals("havanur15")) {
+        if(userName.equals("patika") && password.equals("java101")) {
             System.out.println("Giriş yaptınız !");
         }
-        else if(userName.equals("hrglny") && !password.equals("havanur15")) {
+        else if(userName.equals("patika") && !password.equals("java101")) {
             System.out.print("Şifreniz yanlış. Şifrenizi değiştirmek istiyorsanız 1 e, istemiyorsanız 2 ye basın. ");
             int choice = input.nextInt();
             input.nextLine();
             if(choice == 1) {
                 System.out.print("Yeni şifrenizi giriniz : ");
                 newPassword = input.nextLine();
-                if(newPassword.equals("havanur15") || newPassword.equals(password)) {
+                if(newPassword.equals("java101") || newPassword.equals(password)) {
                     System.out.println("Şifre oluşturulamadı. Başka şifre oluşturunuz.");
                     System.out.print("Şifre : ");
                     newPassword = input.nextLine();
